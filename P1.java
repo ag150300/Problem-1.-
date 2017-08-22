4a2e2b7d9b27d8a21656776961350a95ccbbb58c
ag150300 <ag150556@gmail.com>
2017-08-22 15:08:15 +0800

    20170822

---
+++ P1.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class P1 {
+    public static void main ( String args[] ) {
+        Scanner sc = new Scanner(System.in);
+        int D=0;
+        int sum=0;
+        System.out.print("請輸入數字:");
+        int a = sc.nextInt();
+        String set = Integer.toString(a);
+        int X =set.length();
+    for(int b=1;b<=X;b++) {
+        sum = sum + a % 10;
+        a = a / 10;
+
+    }
+System.out.println(sum);
+
+    for(int b=1;b<=X;b++){
+        char d=set.charAt(D);
+        D=D+1;
+
+        if(b != 5){
+            System.out.print(d+"+");
+       }else{
+            System.out.print(d+"="+sum);
+        }
+    }
+    }
+}
